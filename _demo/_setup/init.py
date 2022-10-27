10ff37490f945342d74f9ec2b3ae6bb59eec8cdf
jakub.vaclavik@datasentics.com <jakub.vaclavik@datasentics.com>
2022-10-27 22:25:34 +0000

    Added join to segment

--- _demo/_setup/init.py
+++ _demo/_setup/init.py
@@ -15,6 +15,7 @@ card_transactions = spark.read.format("parquet").load(f"file://{os.getcwd()}/../
 customer = spark.read.format("parquet").load(f"file://{os.getcwd()}/../_data/customer.parquet")
 web_visits = spark.read.format("parquet").load(f"file://{os.getcwd()}/../_data/web_visits.parquet")
 target_store = spark.read.format("parquet").load(f"file://{os.getcwd()}/../_data/target_store.parquet")
+accounts = spark.read.format("parquet").load(f"file://{os.getcwd()}/../_data/accounts.parquet")
 
 # COMMAND ----------
 
@@ -22,3 +23,4 @@ card_transactions.write.format("delta").mode("overwrite").option("overwriteSchem
 customer.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable("odap_offline_sdm_l2.customer")
 web_visits.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable("odap_digi_sdm_l2.web_visits")
 target_store.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable("odap_targets.targets")
+accounts.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable("odap_features.features_account")
