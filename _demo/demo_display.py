c9560eb3398a45383efc63398d8b2d55bcffed33
KubaVaclavik <88331896+KubaVaclavik@users.noreply.github.com>
2022-10-31 13:53:24 +0100

    Rename demo segment (#6)

--- _demo/demo_display.py
+++ _demo/demo_display.py
@@ -1,4 +1,4 @@
 # Databricks notebook source
 import pandas as pd
 
-pd.read_csv("/dbfs/fake_azure_blob/exports/segments/customers_who_might_be_interested_in_investments.csv")
+pd.read_csv("/dbfs/fake_azure_blob/exports/segments/customer_account_investment_interest.csv")
