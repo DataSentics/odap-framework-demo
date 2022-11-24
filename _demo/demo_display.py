c0403fd0511702a7bf71fe9398e0cd70fc3e0476
karel.gudera@datasentics.com <karel.gudera@datasentics.com>
2022-11-24 08:23:20 +0000

    Fix demo display

--- _demo/demo_display.py
+++ _demo/demo_display.py
@@ -1,4 +1,4 @@
 # Databricks notebook source
 import pandas as pd
 
-pd.read_csv("/dbfs/fake_azure_blob/exports/segments/customer_account_investment_interest.csv")
+pd.read_csv("/dbfs/fake_azure_blob/exports/segments/my_awesome_alert.csv")
