db179faded09afed9f8934ac14b4efe2fcd0430b
samuel.bajanik@datasentics.com <samuel.bajanik@datasentics.com>
2022-12-09 16:33:35 +0000

    further modifications of lookalike train model script

--- segments/lookalike_boosting/train_lookalike_model.py
+++ segments/lookalike_boosting/train_lookalike_model.py
@@ -30,7 +30,7 @@ from pyspark.mllib.evaluation import MulticlassMetrics
 from pyspark.mllib.util import MLUtils
 from pyspark.sql.dataframe import DataFrame
 import pyspark.sql.functions as f
-from segments.lookalike_boosting.ml_functions import lift_curve_colname_specified, ith, process_multiple_segments_input, compute_lift_train_test
+from segments.lookalike_boosting.ml_functions import lift_curve_colname_specified, ith, process_multiple_segments_input, compute_lift_train_test, lift_curve
 import shap
 
 #from pyspark.sql.session import SparkSession
@@ -53,10 +53,10 @@ def get_date_parts(date_string):
 
 dbutils.widgets.text("entity_id_column_name", "")
 dbutils.widgets.dropdown("model_approach", "hyperopt", ["basic", "hyperopt"])
+dbutils.widgets.text("model_type", "")
 dbutils.widgets.dropdown("hyperopt_metric", "lift_10", ["f1", "lift_10", "lift_100", "r2", "rmse", "weighted_precision", "weighted_recall"])
 dbutils.widgets.dropdown("downsample",  "Yes", ["Yes", "No"])
 dbutils.widgets.text("target_ratio", "0.125")
-dbutils.widgets.dropdown("upsample", "Yes", ["Yes", "No"])
 dbutils.widgets.dropdown("normalize", "No", ["Yes", "No"])
 dbutils.widgets.dropdown("normalize_approach", "StandardScaler", ["Normalizer", "StandardScaler", "MinMaxScaler"])
 
@@ -72,7 +72,8 @@ test_data = True
 
 if test_data:
     import pyspark.sql.types as t
-
+    import random
+    
     timestamp = dt.datetime(2022, 9, 30)
 
     schema = t.StructType(
@@ -86,16 +87,7 @@ if test_data:
 
     df_data = spark.createDataFrame(
         [
-        ("1", timestamp, 43, 1), 
-        ("2", timestamp, 32, 0),
-        ("3", timestamp, 26, 1), 
-        ("4", timestamp, 22, 1),
-        ("5", timestamp, 54, 0), 
-        ("6", timestamp, 43, 1),
-        ("7", timestamp, 31, 0), 
-        ("8", timestamp, 29, 1),
-        ("9", timestamp, 20, 0), 
-        ("10", timestamp, 56, 0),
+        (i, timestamp, random.randint(20, 60), i%2) for i in range(100)
         ], 
         schema
     )
@@ -110,11 +102,7 @@ if test_data:
 
     df_to_enrich = spark.createDataFrame(
         [
-        ("xesfoij", "test_segment", "1"),
-        ("xesfoij", "test_segment", "4"),
-        ("xesfoij", "test_segment", "6"),
-        ("xesfoij", "test_segment", "8"),
-        ("xesfoij", "test_segment", "10")
+        ("xesfoij", "test_segment", i*2) for i in range(30)
         ],
         schema_segments
     ).select(dbutils.widgets.get("entity_id_column_name"))
@@ -135,7 +123,7 @@ df_data.join(
 
 # COMMAND ----------
 
-# MAGIC %md #### Feature names
+# MAGIC %md #### Features names
 
 # COMMAND ----------
 
@@ -211,91 +199,7 @@ else:
 
 # COMMAND ----------
 
-#caching
-df_train.cache()
-df_test.cache()
-
-display(df.groupBy('label').count())
-display(df_train.groupBy('label').count())
-display(df_test.groupBy('label').count())}
-
-# COMMAND ----------
-
-# calculate target share and corresponding oversampling ratio
-n_target_1 = df_train.filter(f.col('label') == 1).count()
-n_total = df_train.count()
-actual_target_ratio = n_target_1/n_total
-scale_target_ratio = float(wanted_target_ratio)/actual_target_ratio
-
-smote_perc_over = ((n_total - n_target_1)*scale_target_ratio)/(n_total - scale_target_ratio*n_target_1)
-smote_perc_over = int(smote_perc_over)*100
-
-# no upsampling needed
-if scale_target_ratio <= 1.0 or n_target_1 <= 1000:
-    upsample = "No"
-
-return {
-    'train': df_train,
-    'test': df_test,
-    'smote_perc_over': smote_perc_over,
-    'upsample': upsample,
-    'scaler': scaler,
-}
-
-# COMMAND ----------
-
-# MAGIC %md Smote
-
-# COMMAND ----------
-
-spark.conf
-
-# COMMAND ----------
-
-# make variables visible to scala
-@dp.notebook_function(sampling.result['upsample'])
-def transfer_vars_to_scala(upsample):
-    spark.conf.set('smote_perc_over', sampling.result['smote_perc_over'])
-    spark.conf.set('smote_input_table', define_paths.result['train_sample_location'])
-    spark.conf.set('smote_output_table', define_paths.result['train_sample_smote_location'])
-    spark.conf.set('upsample', upsample)
-
-# COMMAND ----------
-
-# MAGIC %scala
-# MAGIC import org.apache.spark.ml.instance.ASMOTE
-# MAGIC import org.apache.spark.sql.functions
-# MAGIC 
-# MAGIC // variables
-# MAGIC val set_perc_over = spark.conf.get("smote_perc_over").toInt
-# MAGIC val input_table = spark.conf.get("smote_input_table")
-# MAGIC val output_table = spark.conf.get("smote_output_table")
-# MAGIC val upsample = spark.conf.get("upsample")
-# MAGIC 
-# MAGIC if (upsample == "Yes") {
-# MAGIC     // read dataset
-# MAGIC     val ds = spark.read
-# MAGIC                   .format("delta")
-# MAGIC                   .load(input_table)
-# MAGIC                   .withColumn("label", functions.col("label").cast("double"))
-# MAGIC                   .select("label", "features")
-# MAGIC 
-# MAGIC     // using ASMOTE
-# MAGIC     val asmote = new ASMOTE().setK(5)
-# MAGIC                              .setPercOver(set_perc_over)
-# MAGIC                              .setSeed(46)
-# MAGIC     val newDF = asmote.transform(ds)
-# MAGIC 
-# MAGIC     newDF.write.format("delta").mode("overwrite").save(output_table)
-# MAGIC }
-
-# COMMAND ----------
-
-# MAGIC %md #### Model estimation
-
-# COMMAND ----------
-
-# MAGIC %md Load samples
+# MAGIC %md ### Model estimation
 
 # COMMAND ----------
 
@@ -303,9 +207,9 @@ def transfer_vars_to_scala(upsample):
 
 # COMMAND ----------
 
-n_rows = df.count()
+n_rows = df_train.count()
 
-display(df.groupBy('label').count().withColumn('share', f.col('count')/n_rows))
+display(df_train.groupBy('label').count().withColumn('share', f.col('count')/n_rows))
 
 # COMMAND ----------
 
@@ -313,134 +217,115 @@ display(df.groupBy('label').count().withColumn('share', f.col('count')/n_rows))
 
 # COMMAND ----------
 
-def modeling(
-    df_train = read_train
-df_test = read_test
-trackingpoint_ids = process_traits_input.result['table_name_suffix']# FILL IN TABLE NAME SUFFIX
-model_name = dp.get_widget_value("model") # FILL IN MODEL TYPE
-model_approach = dp.get_widget_value("model_approach") # basic, not needed 
-hyperopt_metric = dp.get_widget_value("hyperopt_metric")
-define_paths = define_paths
-assembler = assemble_features.result['assembler']
-sampling_res = sampling.result
-normalize = dp.get_widget_value("normalize")
-normalize_approach = dp.get_widget_value("normalize_approach")
+model_name = dbutils.widgets.get("model_type")
+model_approach = dbutils.widgets.get("model_approach") # basic, not needed 
+hyperopt_metric = dbutils.widgets.get("hyperopt_metric")
 
-):
+
+if model_approach == "basic":
+    MODEL_TYPES = [
+        "LogisticRegression",
+        "SVM",
+        "NaiveBayes",
+        "XGBoost",
+        "DecisionTree",
+        "RandomForest",
+        "MLP",
+    ]
+    CLASSIFIERS = {
+        "LogisticRegression": LogisticRegression(),
+        "XGBoost": GBTClassifier(maxIter=10),
+        "DecisionTree": DecisionTreeClassifier(),
+        "RandomForest": RandomForestClassifier(),
+        "SVM": LinearSVC(),
+        "NaiveBayes": NaiveBayes(modelType="multinomial"),
+        "MLP": MultilayerPerceptronClassifier(layers=[5000, 250, 250, 2]),
+    }
+    
+    
+mlflow.set_experiment(
+    "/odap_segments/"
+)
+
+# Train and log the sklearn model
+with mlflow.start_run():
+    mlflow.log_param("Model_name", "Test_model")
+    mlflow.log_param("Model_type", model_name)
+    mlflow.log_param("Product", "trackingpoint") #do not know what it is
+    
+    # save features names
+    features_names = assembler.getInputCols()
+    with open('/tmp/features.txt', 'w') as out_file:
+        out_file.write(",".join(features_names))
+    mlflow.log_artifact('/tmp/features.txt')
 
     if model_approach == "basic":
-        MODEL_TYPES = [
-            "LogisticRegression",
-            "SVM",
-            "NaiveBayes",
-            "XGBoost",
-            "DecisionTree",
-            "RandomForest",
-            "MLP",
-        ]
-        CLASSIFIERS = {
-            "LogisticRegression": LogisticRegression(),
-            "XGBoost": GBTClassifier(maxIter=10),
-            "DecisionTree": DecisionTreeClassifier(),
-            "RandomForest": RandomForestClassifier(),
-            "SVM": LinearSVC(),
-            "NaiveBayes": NaiveBayes(modelType="multinomial"),
-            "MLP": MultilayerPerceptronClassifier(layers=[5000, 250, 250, 2]),
-        }
+        classifier = CLASSIFIERS[model_name].setFeaturesCol("features").setLabelCol("label")
+        spark_pipeline = Pipeline(stages=[classifier])
+        model_pipeline = spark_pipeline.fit(df_train)
+
+        # fitting a classification model
+        # model_pipeline = fit_supervised_model(
+        #    clf,
+        #    df_train,
+        #    param_space_search="hyperopt",
+        #    max_evals=5,
+        #    metric_name = hyperopt_metric,
+        #    custom_params=custom_params[model_name],
+        #)
         
+    # final model pipeline
+    if dbutils.widgets.get("normalize") == "Yes":
+        print(scaler)
+        model_pipeline_all = PipelineModel(stages = [assembler, scaler, model_pipeline])
+    else:
+        model_pipeline_all = PipelineModel(stages = [assembler, model_pipeline])
         
-    mlflow.set_experiment(
-        "/adpicker/mlflow_model/ADLookalike_model/ADLookalike_TP"
-    )
+
+    m_info = mlflow_spark.log_model(spark_model=model_pipeline_all, artifact_path="Model_test_lookalike")
+
+    # predictions
+    prediction_df = model_pipeline.transform(df_test)
+    prediction_df_train = model_pipeline.transform(df_train)
     
-    # Train and log the sklearn model
-    with mlflow.start_run():
-        mlflow.log_param("Model_name", "Test_model")
-        mlflow.log_param("Model_type", model_name)
-        mlflow.log_param("Product", "trackingpoint") #do not know what it is
+    score_df = prediction_df.withColumn(
+        "score1", ith("probability", f.lit(0))
+    ).withColumn("score2", ith("probability", f.lit(1)))
+    
+    score_df_train = prediction_df_train.withColumn(
+        "score1", ith("probability", f.lit(0))
+    ).withColumn("score2", ith("probability", f.lit(1)))
         
-        # save features names
-        features_names = assembler.getInputCols()
-        with open('/tmp/features.txt', 'w') as out_file:
-            out_file.write(",".join(features_names))
-        mlflow.log_artifact('/tmp/features.txt')
-
-        if model_approach == "basic":
-            classifier = CLASSIFIERS[model_name].setFeaturesCol("features").setLabelCol("label")
-            spark_pipeline = Pipeline(stages=[classifier])
-            model_pipeline = spark_pipeline.fit(df_train)
-
-            # fitting a classification model
-            model_pipeline = fit_supervised_model(
-                clf,
-                df_train,
-                param_space_search="hyperopt",
-                max_evals=5,
-                metric_name = hyperopt_metric,
-                custom_params=custom_params[model_name],
-            )
-            
-        # final model pipeline
-        if normalize == "Yes":
-            scaler = sampling_res['scaler']
-            print(scaler)
-            model_pipeline_all = PipelineModel(stages = [assembler, scaler, model_pipeline])
-        else:
-            model_pipeline_all = PipelineModel(stages = [assembler, model_pipeline])
-            
-
-        m_info = mlflow_spark.log_model(spark_model=model_pipeline_all, artifact_path="Model_" + trackingpoint_ids)
-
-        # predictions
-        prediction_df = model_pipeline.transform(df_test)
-        prediction_df_train = model_pipeline.transform(df_train)
-        
-        score_df = prediction_df.withColumn(
-            "score1", ith("probability", f.lit(0))
-        ).withColumn("score2", ith("probability", f.lit(1)))
-        
-        score_df_train = prediction_df_train.withColumn(
-            "score1", ith("probability", f.lit(0))
-        ).withColumn("score2", ith("probability", f.lit(1)))
-            
-        # lift10 test
-        lift10 = lift_curve_colname_specified(
-            prediction_df, "label", 10, "probability"
-        )
-        lift10.toPandas().to_csv("/tmp/lift10.csv")
-        mlflow.log_artifact("/tmp/lift10.csv")
-        lift10_number = (
-            lift10.filter(f.col("bucket") == 1).select("cum_lift").collect()[0][0]
-        )
-        mlflow.log_param("Lift_10", lift10_number)
-        
-        # lift10 train
-        lift10_train = lift_curve_colname_specified(
-            prediction_df_train, "label", 10, "probability"
-        )
-        lift10_train.toPandas().to_csv('/tmp/lift10_train.csv')
-        mlflow.log_artifact('/tmp/lift10_train.csv')
-        lift10_train_number = lift10_train.filter(f.col('bucket') == 1).select('cum_lift').collect()[0][0]
-        mlflow.log_param('Lift_10_train', lift10_train_number)
-        
-        # lift 100
-        lift100 = lift_curve_colname_specified(
-            prediction_df, "label", 100, "probability"
-        )
-        lift100.toPandas().to_csv("/tmp/lift100.csv")
-        mlflow.log_artifact("/tmp/lift100.csv")
-        lift100_number = (
-            lift100.filter(f.col("bucket") == 1).select("cum_lift").collect()[0][0]
-        )
-        mlflow.log_param("Lift_100", lift100_number)
-        
-    return {
-        'train': score_df_train,
-        'test': score_df,
-        'model_obj': model_pipeline,
-        'model_obj_all': model_pipeline_all,
-        'm_info': m_info,
-    }
+    # lift10 test
+    lift10 = lift_curve_colname_specified(
+        prediction_df, "label", 2, "probability"
+    )
+    lift10.toPandas().to_csv("/tmp/lift10.csv")
+    mlflow.log_artifact("/tmp/lift10.csv")
+    lift10_number = (
+        lift10.filter(f.col("bucket") == 1).select("cum_lift").collect()[0][0]
+    )
+    mlflow.log_param("Lift_10", lift10_number)
+    
+    # lift10 train
+    lift10_train = lift_curve_colname_specified(
+        prediction_df_train, "label", 2, "probability"
+    )
+    lift10_train.toPandas().to_csv('/tmp/lift10_train.csv')
+    mlflow.log_artifact('/tmp/lift10_train.csv')
+    lift10_train_number = lift10_train.filter(f.col('bucket') == 1).select('cum_lift').collect()[0][0]
+    mlflow.log_param('Lift_10_train', lift10_train_number)
+    
+    # lift 100
+    lift100 = lift_curve_colname_specified(
+        prediction_df, "label", 2, "probability"
+    )
+    lift100.toPandas().to_csv("/tmp/lift100.csv")
+    mlflow.log_artifact("/tmp/lift100.csv")
+    lift100_number = (
+        lift100.filter(f.col("bucket") == 1).select("cum_lift").collect()[0][0]
+    )
+    mlflow.log_param("Lift_100", lift100_number)
 
 # COMMAND ----------
 
@@ -448,66 +333,65 @@ normalize_approach = dp.get_widget_value("normalize_approach")
 
 # COMMAND ----------
 
-def prediction_ov_1(df_train: modeling.result['train'], df_test: modeling.result['test'], prob_th=0.5):
-    try:
-        df_train = (df_train
-                    .select('label', f.when(f.col("score2") >= prob_th, 1).otherwise(0).alias("TARGET_PRED"))
-                    .groupBy('label', "TARGET_PRED")
-                    .count()
-                    .sort(f.desc('count'))
-                   )
+prob_th=0.5
+try:
+    df_train_model_testing = (score_df_train
+                .select('label', f.when(f.col("score2") >= prob_th, 1).otherwise(0).alias("TARGET_PRED"))
+                .groupBy('label', "TARGET_PRED")
+                .count()
+                .sort(f.desc('count'))
+                )
 
-        print('train')
-        display(df_train)
+    print('train')
+    display(df_train)
 
-        df_test = (df_test
-                   .select('label', f.when(f.col("score2") >= prob_th, 1).otherwise(0).alias("TARGET_PRED"))
-                   .groupBy('label', "TARGET_PRED")
-                   .count()
-                   .sort(f.desc('count'))
-                  )
+    df_test_model_testing = (score_df
+                .select('label', f.when(f.col("score2") >= prob_th, 1).otherwise(0).alias("TARGET_PRED"))
+                .groupBy('label', "TARGET_PRED")
+                .count()
+                .sort(f.desc('count'))
+                )
 
-        print('test')
-        display(df_test)
+    print('test')
+    display(df_test_model_testing)
 
-        print('accuracy test')
-        n_label_1 = df_test.filter(f.col('label') == 1).groupBy().agg(f.sum('count').alias('count')).select('count').collect()[0][0]
-        n_pred_1 = df_test.filter((f.col('label') == 1) & (f.col('TARGET_PRED') == 1)).groupBy().agg(f.sum('count').alias('count')).select('count').collect()[0][0]
-        print(n_pred_1/n_label_1)
-    except BaseException as e:
-        print(f"ERROR: preiction_ov_1: {e}")
+    print('accuracy test')
+    n_label_1 = df_test_model_testing.filter(f.col('label') == 1).groupBy().agg(f.sum('count').alias('count')).select('count').collect()[0][0]
+    n_pred_1 = df_test_model_testing.filter((f.col('label') == 1) & (f.col('TARGET_PRED') == 1)).groupBy().agg(f.sum('count').alias('count')).select('count').collect()[0][0]
+    print(n_pred_1/n_label_1)
+except BaseException as e:
+    print(f"ERROR: preiction_ov_1: {e}")
 
 # COMMAND ----------
 
-def prediction_ov_2(df_train: modeling.result['train'], df_test: modeling.result['test']):
-    try:
-        n_train = df_train.count()
+try:
+    n_train = score_df_train.count()
 
-        df_train = (df_train
-                        .withColumn('pred', f.round('score2', 2))
-                        .groupBy("pred")
-                        .count()
-                        .sort(f.desc("pred"))
-                        .withColumn('SHARE', f.col('count')/n_train)
-                       )
+    df_train_model_testing = (score_df_train
+                    .withColumn('pred', f.round('score2', 2))
+                    .groupBy("pred")
+                    .count()
+                    .sort(f.desc("pred"))
+                    .withColumn('SHARE', f.col('count')/n_train)
+                    )
 
-        print('train')
-        display(df_train)
+    print('train')
+    display(df_train)
 
-        n_test = df_test.count()
+    n_test = score_df.count()
 
-        df_test = (df_test
-                        .withColumn('pred', f.round('score2', 2))
-                        .groupBy("pred")
-                        .count()
-                        .sort(f.desc("pred"))
-                        .withColumn('SHARE', f.col('count')/n_test)
-                       )
+    df_test_model_testing = (score_df
+                    .withColumn('pred', f.round('score2', 2))
+                    .groupBy("pred")
+                    .count()
+                    .sort(f.desc("pred"))
+                    .withColumn('SHARE', f.col('count')/n_test)
+                    )
 
-        print('test')
-        display(df_test)
-    except BaseException as e:
-        print(f"ERROR: preiction_ov_2: {e}")
+    print('test')
+    display(df_test_model_testing)
+except BaseException as e:
+    print(f"ERROR: preiction_ov_2: {e}")
 
 # COMMAND ----------
 
@@ -516,11 +400,10 @@ def prediction_ov_2(df_train: modeling.result['train'], df_test: modeling.result
 
 # COMMAND ----------
 
-def get_lift(df_train = modeling.result['train'], df_test = modeling.result['test']):
-    try:
-        display(compute_lift_train_test(df_train, df_test, "label").withColumnRenamed('lift_train', 'Lift Train').withColumnRenamed('lift_test', 'Lift Test'))
-    except BaseException as e:
-        print(f"ERROR: lift: {e}")
+try:
+    display(compute_lift_train_test(score_df_train, score_df, "label", "features").withColumnRenamed('lift_train', 'Lift Train').withColumnRenamed('lift_test', 'Lift Test'))
+except BaseException as e:
+    print(f"ERROR: lift: {e}")
 
 # COMMAND ----------
 
